d86b614f01482c37159f755e0dee695d27d2c3a9
Arpan Sinha <itsarpansinha@hotmail.com>
2025-10-08 13:35:33 +0530

    Complete Singly Linked List

--- SinglyLinkedList.c
+++ SinglyLinkedList.c
@@ -55,3 +55,218 @@ Node* insertAtLast(Node* head,int data)
     printf("\nInserted Successfully\n");
     return head;
 }
+
+int count(Node *head)
+{
+    int nodeCount=0;
+    while(head!=NULL)
+    {
+        nodeCount++;
+        head=head->next;
+    }
+    return nodeCount;
+}
+
+Node* deleteAtFirst(Node* head)
+{
+    Node* temp=head;
+    if(!head)
+    {
+        printf("\nList Is Empty\n");
+        return NULL;
+    }
+    else if(!head->next)
+    {
+        free(temp);
+        printf("\nDeleted Successfuly\n");
+        return NULL;
+    }
+    else
+    {
+        head=head->next;
+        free(temp);
+        printf("\nDeleted Successfuly\n");
+    }
+    return head;
+}
+
+Node* deleteAtLast(Node* head)
+{
+    Node* temp=head;
+    Node* tail=NULL;
+    if(!head)
+    {
+        printf("\nList Is Empty\n");
+        return NULL;
+    }
+    else
+    {
+        while(temp->next)
+        {
+            tail=temp;
+            temp=temp->next;
+        }
+        tail->next=NULL;
+        free(temp);
+        printf("\nDeleted Successfuly\n");
+    }
+    return head;
+}
+
+Node *deleteAtPosition(Node *head,int index)
+{
+    int position=0;
+    Node *temp=head;
+    Node *temp2=NULL;
+    if(head==NULL)
+    {
+        printf("Linked List is empty\n");
+    }
+    else if(index<0||index>count(head)-1)
+    {
+        printf("Invalid Index! Valid range (0 to %d)\n",count(head)-1);
+    }
+    else
+    {
+    while(position<index)
+    {
+        position++;
+        temp2=temp;
+        temp=temp->next;
+        
+    }
+    temp2->next=temp->next;
+    free(temp);
+    printf("\nDeleted Successfuly\n");
+    }
+    return head;
+}
+
+Node *insertAtPosition(Node *head,int data,int index)
+{
+    if(head==NULL)
+    {
+        printf("\nLinked List is empty\n");
+    }
+    else if(index>=count(head)+1||index<0)
+    {
+        printf("\nInvalid Index! Valid range (0 to %d)\n",count(head));
+    }
+    else
+    {
+    Node *newNode=createNode(data);
+    Node *temp=head;
+    int position=0;
+    while(position<index-1)
+    {
+        position++;
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    printf("\nInserted Successfully\n");
+    }
+    return head;
+}
+
+Node *reverseList(Node *head)
+{
+    Node *temp=head;
+    Node *fast=NULL;
+    Node *slow=NULL;
+    if(head==NULL)
+    {
+        printf("\nLinked List is empty\n");
+    }
+    else
+    {
+    while(temp!=NULL)
+    {
+        fast=temp->next;
+        temp->next=slow;
+        slow=temp;
+        temp=fast;
+    }
+    head=slow;
+    printf("\nReversed Successfully\n");
+    }
+    return head;
+}
+
+void displayList(Node* head)
+{
+    if(!head)
+    {
+        printf("\nNothing to display\n");
+    }
+    else
+    {
+        while(head)
+        {
+            printf("[%d] -> ",head->data);
+            head=head->next;
+        }
+        printf("NULL");
+    }
+}
+
+int main()
+{
+    int data,choice,pos;
+    Node* head=NULL;
+    do
+    {
+        printf("\n\tMENU\t\n");
+        printf("1.)Insert At First\n");
+        printf("2.)Insert At Last\n");
+        printf("3.)Delete At First\n");
+        printf("4.)Delete At Last\n");
+        printf("5.)Insert At Position\n");
+        printf("6.)Delete At Position\n");
+        printf("7.)Reverse List\n");
+        printf("8.)Number of Nodes\n");
+        printf("9.)Display Linked List\n");
+        printf("10.)Exit\n");
+        printf("Enter your choice :");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1:printf("Enter data to insert :");
+            scanf("%d",&data);
+            head=insertAtFirst(head,data);break;
+
+            case 2:printf("Enter data to insert :");
+            scanf("%d",&data);
+            head=insertAtLast(head,data);break;
+
+            case 3:head=deleteAtFirst(head);break;
+
+            case 4:head=deleteAtLast(head);break;
+
+            case 5:printf("Enter data to insert :");
+            scanf("%d",&data);
+            printf("Enter position :");
+            scanf("%d",&pos);
+            head=insertAtPosition(head,data,pos);break;
+
+            case 6:printf("Enter position to delete :");
+            scanf("%d",&pos);
+            head=deleteAtPosition(head,pos);break;
+
+            case 7:head=reverseList(head);break;
+
+            case 8:printf("\nTotal Number of Nodes :%d\n",count(head));break;
+
+            case 9:displayList(head);break;
+
+            case 10:printf("\nExiting...\n");break;
+
+            default:printf("\nInvalid Input! Please enter a valid input (1,10)\n");
+
+
+        }
+    } while (choice!=10);
+    
+    return 0;
+}
